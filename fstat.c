a60411a9c81622ee04dc7d282b88ff8a52cdcd2f
neogeoss <neogeo-s@hanmail.net>
2019-01-13 21:50:23 +0900

    file structure and standard input use

---
+++ fstat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main() 
+{
+    int ret = 0;
+    struct stat buf;
+    ret = stat("link.txt", &buf);
+    if(ret < 0 ) 
+    {
+        printf("Error\n");
+        return 0;
+    }
+
+    printf("Device ID[%ld], inodeNum[%ld], hardlinkCount[%ld], filesize[%ld], blocksize[%ld], blockcount[%ld] \n", buf.st_dev, buf.st_ino, buf.st_nlink, buf.st_size, buf.st_blksize, buf.st_blocks);
+}
